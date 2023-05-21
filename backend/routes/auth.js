030b53b4fc9f359b205c7b2b59adc89e4ed32635
bhushanK <bhushank.bwd@gmail.com>
2023-05-21 19:41:09 +0530

    validation done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,8 +6,28 @@ const j = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-router.post("/register", async (req, res) => {
-  console.log(req.body);
-});
+router.post(
+  "/register",
+  [
+    body("username")
+      .notEmpty()
+      .withMessage("Enter a username")
+      .isLength({ min: 3 })
+      .withMessage("Enter a username(min 3 chars)"),
+    body("email", "Enter a valid email address").isEmail(),
+    body("phoneno", "Enter a valid phone number (8 to 13 digits)").matches(
+      /^[0-9]{8,13}$/
+    ),
+  ],
+  async (req, res) => {
+    let status = false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ status: status, errors: errors.array() });
+    }
+    
+    console.log("validation done");
+    return res.status(400).json({ status: true, data:req.body });
+  }
+);
 module.exports = router;
-
