17a71c5819d7d37b1b9a9fef7771e07a0c4d50ba
bhushanK <bhushank.bwd@gmail.com>
2023-05-21 19:18:51 +0530

    express-validator,bcryptjs, jsonwebtoken installed and auth router included in index.js and simple register endpoint created

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const app = express();
 const port = 5000;
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
