9f636fdd8c3d9e7bacc2d2ba7588c57bf3ac2c5b
bhushanK <bhushank.bwd@gmail.com>
2023-05-28 19:02:58 +0530

    loader on home & check uses of consumer in Context API

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSite } from "../../context/Site";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const siteState = useSite();
+  useEffect(() => {
+    siteState.setProgress(100);
+     // eslint-disable-next-line 
+  }, []);
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
